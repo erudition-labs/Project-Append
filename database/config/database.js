389b16a42e0edcda4398cf1f206e9a5c743077c0
Joshua Benz <joshua@planethull.com>
2018-06-01 04:29:32 -0700

    database config settings

---
+++ database/config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+    database: 'mongodb://localhost:27017/UmbrellaIrrigation',
+    secret: 'lzmkKcnbfinngfeloifnlcrkymfkehlkemfkqsthlamacpd}mho`gtrojx(/mcrf' //use some randomly constantly changing ENV var for this in production
+}
