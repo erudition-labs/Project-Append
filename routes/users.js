375656772a71a3c0c80bda40102335bf6ecf70ae
Joshua Benz <joshua@planethull.com>
2018-06-02 08:23:10 -0700

    added more validation

--- routes/users.js
+++ routes/users.js
@@ -1,27 +1,30 @@
 //Add all routes that users can access here
-const express   = require('express');
-const User         = require('../database/models/user');
-const passport  = require('passport');
-const jwt            = require('jsonwebtoken');
+const express                                           = require('express');
+const User                                                = require('../database/models/user');
+const passport                                         = require('passport');
+const jwt                                                    = require('jsonwebtoken');
 const { body, check, validationResult } = require('express-validator/check');
-const { sanitizeBody } = require('express-validator/filter');
-const config      = require('../database/config/database');
-const router      = express.Router();
+const { sanitizeBody }                              = require('express-validator/filter');
+const config                                               = require('../database/config/database');
+const router                                               = express.Router();
 
 /////////////////////add passport.authenticate('jwt', {session: false}) as a parameter to protect a route.
 
 //Register
 router.post('/register', [
+    // validate and sanitize any fields from the client
+    // Note that this does in place field mutation
     body('email').isEmail().withMessage('Valid Email Required').normalizeEmail(),
     body('firstName').not().isEmpty().withMessage('First Name Required').trim().escape(),
     body('lastName').not().isEmpty().withMessage('Last Name Required').trim().escape(),
     body('rank').not().isEmpty().withMessage('Rank Required').trim().escape() //for now just sanitize rank, eventually we can check if it is a valid rank
 ], (request, response, next) => {
+    // check for any errors from above
     const errors = validationResult(request);
-    if (!errors.isEmpty()) {
+    if (!errors.isEmpty()) { // If there are any, respond with them
     return response.status(422).json({ errors: errors.array() });
   }
-
+    // otherwise create a user object based off of our schema
     let newUser = new User({
         firstName    : request.body.firstName,
         lastName    : request.body.lastName,
@@ -30,6 +33,7 @@ router.post('/register', [
         password   : request.body.password
     });
 
+    // add user to database
     User.addUser(newUser, (error, user) => {
         if(error) {
             response.json({ success: false, msg:'Failed To Register User'});
@@ -39,18 +43,29 @@ router.post('/register', [
     });
 });
 
-//Authenticate
-router.post('/authenticate', (request, response, next) => {
-    const username = request.body.username;
+// Authenticate user and return a token if valid
+router.post('/authenticate', [
+    //validate and sanitize
+    body('email').isEmail().withMessage('Valid Email Required').normalizeEmail()
+], (request, response, next) => {
+    // Check for validation/sanitization errors
+    const errors = validationResult(request);
+    if (!errors.isEmpty()) { // If there are any, respond with them
+    return response.status(422).json({ errors: errors.array() });
+  }
+
+  //otherwise continue
+    const email = request.body.email;
     const password = request.body.password;
 
-    User.findUserByEmail(username, (error, user) => {
+    User.findUserByEmail(email, (error, user) => {
         if(error) throw error;
         if(!user) {
             return response.json({success: false, msg: 'User not found :('});
-        } //when user is not in db
+        } // when user is not in db
 
-        //otherwise assume it is, check the rest of the stuff
+        // otherwise assume it is, check the rest of the stuff
+        // Note that we are comparing hashed passwords
         User.comparePassword(password, user.password, (error, isMatch) => {
             if(error) throw error;
             if(isMatch) {
@@ -77,7 +92,7 @@ router.post('/authenticate', (request, response, next) => {
     });
 });
 
-
+// return array of all users in users collection
 router.get('/users', (request, response, next) => {
     User.findAllUsers((error, users) => {
         if(error) {
@@ -93,8 +108,7 @@ router.get('/users', (request, response, next) => {
     });
 });
 
-
-
+//delete user by id
 router.delete('/users/:id', (request, response, next) => {
     User.finAndDeleteUserById(request.params.id, (error, user) => {
         if(error) {
@@ -105,12 +119,9 @@ router.delete('/users/:id', (request, response, next) => {
     });
 });
 
-
 //Profile
 router.get('/profile', passport.authenticate('jwt', {session: false}), (request, response, next) => {
     response.json({user:request.user});
 });
-
-
 
 module.exports = router;
