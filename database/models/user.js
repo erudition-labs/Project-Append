54df824cdb077bc01f7b628610f757712fc10739
Tristan Hull <soldier@kenpokicks.com>
2018-06-07 15:02:21 -0700

    fixed two spaces

--- database/models/user.js
+++ database/models/user.js
@@ -4,7 +4,7 @@ const uniqueValidator      = require('mongoose-unique-validator'); //for unique 
 
 const UserSchema = mongoose.Schema({
     firstName    : { type: String, required: true },
-    lastName     : {type: String, required: true },
+    lastName     : { type: String, required: true },
     email        : { type: String, index: true, unique: true, required: true },
     rank         : { type: String, required: true },
     flight       : { type: String, required: true},
