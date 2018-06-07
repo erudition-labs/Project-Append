04739b3ba133bda5ca24847f4b3a2b4b15a0dc42
Joshua Benz <joshua@planethull.com>
2018-06-07 04:12:33 -0700

    handled nebulars existence...violently

--- angular-src/src/app/app.component.ts
+++ angular-src/src/app/app.component.ts
@@ -1,21 +1,37 @@
-/**
- * @license
- * Copyright Akveo. All Rights Reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- */
-import { Component, OnInit } from '@angular/core';
-import { AnalyticsService } from './@core/utils/analytics.service';
+import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+
+import { FuseSplashScreenService } from '@fuse/services/splash-screen.service';
+import { FuseTranslationLoaderService } from '@fuse/services/translation-loader.service';
+import { FuseNavigationService } from '@fuse/components/navigation/navigation.service';
+
+import { locale as navigationEnglish } from './navigation/i18n/en';
+import { locale as navigationTurkish } from './navigation/i18n/tr';
 
 @Component({
-  selector: 'ngx-app',
-  template: '<router-outlet></router-outlet>',
+    selector   : 'fuse-root',
+    templateUrl: './app.component.html',
+    styleUrls  : ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent
+{
+    constructor(
+        private translate: TranslateService,
+        private fuseNavigationService: FuseNavigationService,
+        private fuseSplashScreen: FuseSplashScreenService,
+        private fuseTranslationLoader: FuseTranslationLoaderService
+    )
+    {
+        // Add languages
+        this.translate.addLangs(['en', 'tr']);
 
-  constructor(private analytics: AnalyticsService) {
-  }
+        // Set the default language
+        this.translate.setDefaultLang('en');
 
-  ngOnInit(): void {
-    this.analytics.trackPageViews();
-  }
+        // Set the navigation translations
+        this.fuseTranslationLoader.loadTranslations(navigationEnglish, navigationTurkish);
+
+        // Use a language
+        this.translate.use('en');
+    }
 }
--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,36 +1,80 @@
-/**
- * @license
- * Copyright Akveo. All Rights Reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- */
-import { APP_BASE_HREF } from '@angular/common';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { CoreModule } from './@core/core.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { TranslateModule } from '@ngx-translate/core';
+import 'hammerjs';
+
+import { FuseModule } from '@fuse/fuse.module';
+import { FuseSharedModule } from '@fuse/shared.module';
+
+import { fuseConfig } from './fuse-config';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { ThemeModule } from './@theme/theme.module';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FuseFakeDbService } from './fuse-fake-db/fuse-fake-db.service';
+import { FuseMainModule } from './main/main.module';
+import { AppStoreModule } from './store/store.module';
+
+const appRoutes: Routes = [
+    {
+        path        : 'apps',
+        loadChildren: './main/content/apps/apps.module#FuseAppsModule'
+    },
+    {
+        path        : 'pages',
+        loadChildren: './main/content/pages/pages.module#FusePagesModule'
+    },
+    {
+        path        : 'ui',
+        loadChildren: './main/content/ui/ui.module#FuseUIModule'
+    },
+    {
+        path        : 'services',
+        loadChildren: './main/content/services/services.module#FuseServicesModule'
+    },
+    {
+        path        : 'components',
+        loadChildren: './main/content/components/components.module#FuseComponentsModule'
+    },
+    {
+        path        : 'components-third-party',
+        loadChildren: './main/content/components-third-party/components-third-party.module#FuseComponentsThirdPartyModule'
+    },
+    {
+        path      : '**',
+        redirectTo: 'apps/dashboards/analytics'
+    }
+];
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    HttpClientModule,
-    AppRoutingModule,
+    declarations: [
+        AppComponent
+    ],
+    imports     : [
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        RouterModule.forRoot(appRoutes),
 
-    NgbModule.forRoot(),
-    ThemeModule.forRoot(),
-    CoreModule.forRoot(),
-  ],
-  bootstrap: [AppComponent],
-  providers: [
-    { provide: APP_BASE_HREF, useValue: '/' },
-  ],
+        TranslateModule.forRoot(),
+        InMemoryWebApiModule.forRoot(FuseFakeDbService, {
+            delay             : 0,
+            passThruUnknownUrl: true
+        }),
+
+        // Fuse Main and Shared modules
+        FuseModule.forRoot(fuseConfig),
+        FuseSharedModule,
+
+        AppStoreModule,
+        FuseMainModule
+    ],
+    bootstrap   : [
+        AppComponent
+    ]
 })
-export class AppModule {
+export class AppModule
+{
 }
