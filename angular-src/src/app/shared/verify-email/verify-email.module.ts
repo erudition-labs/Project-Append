d7cf2947c591c46dcbef0529e95d7a44b5b80477
Joshua Benz <joshua@planethull.com>
2018-06-14 08:23:37 -0700

    setup email verification module

--- angular-src/src/app/shared/verify-email/verify-email.module.ts
+++ angular-src/src/app/shared/verify-email/verify-email.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 import { VerifyEmailComponent } from './verify-email.component';
 
+const routes : Routes = [
+	{ path: '', component: VerifyEmailComponent }
+];
+
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [VerifyEmailComponent]
+	imports: [
+		CommonModule,
+		RouterModule.forChild(routes)
+	],
+	providers: [
+		AuthService
+	],
+	declarations: [VerifyEmailComponent]
 })
 export class VerifyEmailModule { }
