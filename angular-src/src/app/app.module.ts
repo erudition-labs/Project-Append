5ec43e8f59ef8944704351b41ca0700e9b0a97c3
Joshua Benz <joshua@planethull.com>
2018-06-14 09:36:56 -0700

    attempt some sort of layout

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -4,16 +4,31 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatGridListModule, MatCardModule, MatMenuModule } from '@angular/material';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
 	BrowserAnimationsModule,
 	HttpClientModule,
-	AppRoutingModule
+	AppRoutingModule,
+	LayoutModule,
+	MatToolbarModule,
+	MatButtonModule,
+	MatSidenavModule,
+	MatIconModule,
+	MatListModule,
+	MatGridListModule,
+	MatCardModule,
+	MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-src/src/app/shared/login/login.module.ts
+++ angular-src/src/app/shared/login/login.module.ts
@@ -3,6 +3,10 @@ import { CommonModule 			} from '@angular/common';
 import { RouterModule, Routes 	} from '@angular/router';
 import { LoginComponent 		} from './login.component';
 import { AuthService 			} from '../../services/auth.service';
+import { MatInputModule 		} from '@angular/material/input';
+import { MatFormFieldModule 	} from '@angular/material/form-field';
+import { MatCardModule 			} from '@angular/material/card';
+import { MatButtonModule 		} from '@angular/material/button';
 
 const routes : Routes = [
 	{ path: '', component: LoginComponent}
@@ -11,6 +15,10 @@ const routes : Routes = [
 @NgModule({
 	imports: [
 		CommonModule,
+		MatInputModule,
+		MatFormFieldModule,
+		MatCardModule,
+		MatButtonModule,
 		RouterModule.forChild(routes)
 	],
 	providers: [
