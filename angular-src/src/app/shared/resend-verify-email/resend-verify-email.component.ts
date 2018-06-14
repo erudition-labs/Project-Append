39ccba952def626ee70ffaf8f77aa655e25a8870
Joshua Benz <joshua@planethull.com>
2018-06-14 08:29:08 -0700

    resend verification component and module setup

---
+++ angular-src/src/app/shared/resend-verify-email/resend-verify-email.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resend-verify-email',
+  templateUrl: './resend-verify-email.component.html',
+  styleUrls: ['./resend-verify-email.component.css']
+})
+export class ResendVerifyEmailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- angular-src/src/app/shared/resend-verify-email/resend-verify-email.module.ts
+++ angular-src/src/app/shared/resend-verify-email/resend-verify-email.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { RegisterComponent } from './register.component';
+import { ResendVerifyEmailComponent } from './resend-verify-email.component';
 import { AuthService } from '../../services/auth.service';
 
 const routes : Routes = [
-	{ path: '', component: RegisterComponent }
+	{ path: '', component: ResendVerifyEmailComponent }
 ];
 
 @NgModule({
 	imports: [
 		CommonModule,
 		RouterModule.forChild(routes)
-	],
+	], 
 	providers: [
 		AuthService
 	],
-	declarations: [RegisterComponent]
+	declarations: [ResendVerifyEmailComponent]
 })
-export class RegisterModule { }
+export class ResendVerifyEmailModule { }
