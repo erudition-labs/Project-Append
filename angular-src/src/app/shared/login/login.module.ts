d4550de86adf8b3e4522db16f17e446e874f0095
Joshua Benz <joshua@planethull.com>
2018-06-14 08:21:00 -0700

    setup login module

--- angular-src/src/app/shared/login/login.module.ts
+++ angular-src/src/app/shared/login/login.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login.component';
+import { AuthService } from '../../services/auth.service';
+
+const routes : Routes = [
+	{ path: '', component: LoginComponent}
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: [LoginComponent]
+	imports: [
+		CommonModule,
+		RouterModule.forChild(routes)
+	],
+	providers: [
+		AuthService
+	],
+	declarations: [LoginComponent]
 })
 export class LoginModule { }
