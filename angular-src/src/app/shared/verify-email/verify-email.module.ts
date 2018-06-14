fafd9f66723394f7dadf7b4d55e19f79811c0376
Joshua Benz <joshua@planethull.com>
2018-06-14 08:17:11 -0700

    added other auth pages

---
+++ angular-src/src/app/shared/verify-email/verify-email.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VerifyEmailComponent } from './verify-email.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [VerifyEmailComponent]
+})
+export class VerifyEmailModule { }
