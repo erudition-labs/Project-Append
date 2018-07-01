3691f7e2afc39e16d8b5db79f722b4c258937110
Joshua Benz <joshua@planethull.com>
2018-07-01 05:49:16 -0700

    form settings

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -3,19 +3,29 @@
  * Copyright Akveo. All Rights Reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
-import { APP_BASE_HREF } from '@angular/common';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { CoreModule } from './@core/core.module';
+import { APP_BASE_HREF 				} from '@angular/common';
+import { BrowserModule 				} from '@angular/platform-browser';
+import { BrowserAnimationsModule 	} from '@angular/platform-browser/animations';
+import { NgModule 					} from '@angular/core';
+import { HttpClientModule 			} from '@angular/common/http';
+import { CoreModule 				} from './@core/core.module';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { ThemeModule } from './@theme/theme.module';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppComponent 			} from './app.component';
+import { AppRoutingModule 		} from './app-routing.module';
+import { ThemeModule 			} from './@theme/theme.module';
+import { NgbModule 				} from '@ng-bootstrap/ng-bootstrap';
 import { NbPasswordAuthStrategy, NbAuthModule } from '@nebular/auth';
 
+
+const formSettings : any = {
+	redirectDelay: 500,  // delay before redirect after a successful login, while success message is shown 
+	strategy: 'email',  // strategy id key
+	rememberMe: false, // whether to show or not the `rememberMe` checkbox
+	showMessages: { // show/not show success/error messages
+		success: true,
+		error: true,
+	},
+};
 
 @NgModule({
   declarations: [AppComponent],
@@ -44,7 +54,10 @@ import { NbPasswordAuthStrategy, NbAuthModule } from '@nebular/auth';
 				},
 			}),
 		],
-		forms: {},
+		forms: {
+			login 		: formSettings,
+			register	: formSettings,	
+		},
 	}), 
   ],
   bootstrap: [AppComponent],
