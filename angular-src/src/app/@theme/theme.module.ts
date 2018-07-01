005c71457fa317e7350d023f41201b6fd7dc8dae
Joshua Benz <joshua@planethull.com>
2018-07-01 07:19:23 -0700

    @theme module now loads custom auth UI modules

--- angular-src/src/app/@theme/theme.module.ts
+++ angular-src/src/app/@theme/theme.module.ts
@@ -1,8 +1,17 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NgxRegisterComponent } from './components/auth/register/register.component';
+import { ModuleWithProviders, NgModule 		} from '@angular/core';
+import { CommonModule 						} from '@angular/common';
+import { FormsModule, ReactiveFormsModule 	} from '@angular/forms';
+import { NgbModule 							} from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule 						} from '@angular/router';
+
+import { NgxAuthComponent 				} from './components/auth/auth.component';
+import { NgxAuthBlockComponent 			} from './components/auth/auth-block/auth-block.component';
+import { NgxLoginComponent 				} from './components/auth/login/login.component';
+import { NgxRegisterComponent 			} from './components/auth/register/register.component';
+import { NgxLogoutComponent 			} from './components/auth/logout/logout.component';
+import { NgxRequestPasswordComponent 	} from './components/auth/request-password/request-password.component';
+import { NgxResetPasswordComponent 		} from './components/auth/reset-password/reset-password.component';
+
 
 
 import {
@@ -45,7 +54,7 @@ import { DEFAULT_THEME } from './styles/theme.default';
 import { COSMIC_THEME } from './styles/theme.cosmic';
 import { CORPORATE_THEME } from './styles/theme.corporate';
 
-const BASE_MODULES = [CommonModule, FormsModule, ReactiveFormsModule];
+const BASE_MODULES = [CommonModule, FormsModule, ReactiveFormsModule, RouterModule];
 
 const NB_MODULES = [
   NbCardModule,
@@ -78,6 +87,13 @@ const COMPONENTS = [
   SampleLayoutComponent,
   ThreeColumnsLayoutComponent,
   TwoColumnsLayoutComponent,
+  NgxAuthComponent,
+  NgxAuthBlockComponent,
+  NgxLoginComponent,
+  NgxRegisterComponent,
+  NgxRequestPasswordComponent,
+  NgxResetPasswordComponent,
+  NgxLogoutComponent,
 ];
 
 const ENTRY_COMPONENTS = [
@@ -105,9 +121,7 @@ const NB_THEME_PROVIDERS = [
 @NgModule({
 	imports: [...BASE_MODULES, ...NB_MODULES],
 	exports: [...BASE_MODULES, ...NB_MODULES, ...COMPONENTS, ...PIPES],
-	declarations: [...COMPONENTS, ...PIPES,
-		NgxRegisterComponent,	
-	],
+	declarations: [...COMPONENTS, ...PIPES],
 	entryComponents: [...ENTRY_COMPONENTS],
 })
 export class ThemeModule {
