a57cb716f459bac0cec8d85ae0f57a0ef16f6786
Joshua Benz <joshua@planethull.com>
2018-07-07 16:38:07 -0700

    email verification

--- angular-src/src/app/@theme/components/auth/email-verification/email-verification.component.ts
+++ angular-src/src/app/@theme/components/auth/email-verification/email-verification.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { AuthService } from '../../../../@core/data/auth.service';
 
 
@@ -10,12 +10,21 @@ import { AuthService } from '../../../../@core/data/auth.service';
 })
 export class EmailVerificationComponent implements OnInit {
 
-	constructor(private router		: Router,
-				private authService : AuthService) { }
+	constructor(private router				: Router,
+				private route 				: ActivatedRoute,
+				private authService 		: AuthService) { }
 
 	ngOnInit() {
-  		let code : string = this.router.url;
-		console.log(code);
+	//let href : string = this.router.url;
+	//	console.log(href.split("/", 2));
+	this.route.params.subscribe( (params) => {
+	//console.log(params) 
+		this.authService.verify(params.code).subscribe((result) => {
+			return this.router.navigateByUrl("/");
+		});
+
+	});
+
 	}
 
 }
