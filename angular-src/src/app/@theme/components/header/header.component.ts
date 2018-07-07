a5968fed729580b3d067b44838ae4d6091582f44
Joshua Benz <joshua@planethull.com>
2018-07-07 13:10:59 -0700

    cleaned up

--- angular-src/src/app/@theme/components/header/header.component.ts
+++ angular-src/src/app/@theme/components/header/header.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnInit 			} from '@angular/core';
 import { NbAuthJWTToken, NbAuthService 		} from '@nebular/auth';
 import { NbMenuService, NbSidebarService 	} from '@nebular/theme';
-import { UserService 						} from '../../../@core/data/users.service';
 import { AnalyticsService 					} from '../../../@core/utils/analytics.service';
 
 @Component({
@@ -20,7 +19,6 @@ export class HeaderComponent implements OnInit {
 
 	constructor(private sidebarService: NbSidebarService,
               private menuService: NbMenuService,
-              private userService: UserService,
 			  private authService: NbAuthService,
 			  private analyticsService: AnalyticsService) {
 
