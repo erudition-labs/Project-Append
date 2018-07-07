bf02ea2913ed5a4d120e5d638933a647d32ce18f
Joshua Benz <joshua@planethull.com>
2018-07-07 14:00:47 -0700

    capture menu events

--- angular-src/src/app/app.component.ts
+++ angular-src/src/app/app.component.ts
@@ -3,8 +3,9 @@
  * Copyright Akveo. All Rights Reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
-import { Component, OnInit } from '@angular/core';
-import { AnalyticsService } from './@core/utils/analytics.service';
+import { Component, OnInit 		} from '@angular/core';
+import { AnalyticsService 		} from './@core/utils/analytics.service';
+import { NbMenuService 			} from '@nebular/theme';
 
 @Component({
   selector: 'ngx-app',
@@ -12,10 +13,22 @@ import { AnalyticsService } from './@core/utils/analytics.service';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private analytics: AnalyticsService) {
-  }
+constructor(private analytics: AnalyticsService,
+			private menuService: NbMenuService) {
 
-  ngOnInit(): void {
-    this.analytics.trackPageViews();
-  }
+	this.menuService.onItemClick()
+		.subscribe((event) => {
+			this.onContecxtItemSelection(event.item.title);
+ 		});
+	}
+
+	ngOnInit(): void {
+		this.analytics.trackPageViews();
+	}
+
+	onContecxtItemSelection(title) {
+		if(title === 'Log out') {
+			
+		}
+	}
 }
