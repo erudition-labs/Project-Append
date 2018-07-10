937a5829c00598369681598a6ad8f597d5ce6965
Tristan <soldier@kenpokicks.com>
2018-07-09 20:57:38 -0700

    profile component

--- angular-src/src/app/pages/pages.module.ts
+++ angular-src/src/app/pages/pages.module.ts
@@ -5,6 +5,7 @@ import { DashboardModule } from './dashboard/dashboard.module';
 import { PagesRoutingModule } from './pages-routing.module';
 import { ThemeModule } from '../@theme/theme.module';
 import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
+import { ProfileComponent } from './profile/profile.component';
 
 const PAGES_COMPONENTS = [
   PagesComponent,
@@ -19,6 +20,7 @@ const PAGES_COMPONENTS = [
   ],
   declarations: [
     ...PAGES_COMPONENTS,
+    ProfileComponent,
   ],
 })
 export class PagesModule {
