9b3e76da55a62f93ccbe7654ee2c521c89fa4b02
Joshua Benz <joshua@planethull.com>
2018-08-07 21:22:25 -0700

    fixed controller name

--- api_v2/api/users/index.js
+++ api_v2/api/users/index.js
@@ -2,7 +2,7 @@ const express 		= require('express');
 const router 		= express.Router();
 const controller 	= require('./controller');
 
-router.route('/').post(Controller.postUser);
+router.route('/').post(controller.postUser);
 router.route('/check-email').get(controller.getUserByEmail);
 
 module.exports = router;
