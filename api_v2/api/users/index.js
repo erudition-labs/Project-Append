5fa577ad386fd4c027263d20db5e4c4b6bd8cbf1
Joshua Benz <joshua@planethull.com>
2018-08-07 22:01:56 -0700

    added validators and sanitization

--- api_v2/api/users/index.js
+++ api_v2/api/users/index.js
@@ -1,8 +1,21 @@
-const express 		= require('express');
-const router 		= express.Router();
-const controller 	= require('./controller');
+const express 									= require('express');
+const router 									= express.Router();
+const controller 								= require('./controller');
+const { body, check, validationResult }			= require('express-validator/check');
+const { sanitizeBody }							= require('express-validator/filter');
 
-router.route('/').post(controller.postUser);
+
+router.route('/').post([
+	body('email').exists().isEmail().withMessage('Valid Email Required').normalizeEmail(),
+	body('firstName').exists().withMessage('First Name Required').trim().escape(),
+	body('lastName').exists().withMessage('Last Name Required').trim().escape(),
+	body('rank').exists().withMessage('Rank Required').trim().escape(),
+	body('flight').exists().withMessage('Flight Required').trim().escape(),
+	body('team').exists().withMessage('Team Required').trim().escape(),
+	body('password').exists().withMessage('Password Required').trim().escape()
+], controller.postUser);
+
+
 router.route('/check-email').get(controller.getUserByEmail);
 
 module.exports = router;
