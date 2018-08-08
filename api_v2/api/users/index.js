ea31a124ef7794cd1293b08206f7cff7c8123e7a
Joshua Benz <joshua@planethull.com>
2018-08-07 20:54:43 -0700

    controller routes

---
+++ api_v2/api/users/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router express.Router();
+const controller = require('./controller');
+
+router.route('/').post(Controller.postUser);
+router.route('/check-email').get(controller.getUserByEmail);
+
+module.exports router;
