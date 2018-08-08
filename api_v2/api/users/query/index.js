ec63d32843f5c9534e1d02a2dc7aa6f6718ff584
Joshua Benz <joshua@planethull.com>
2018-08-07 20:25:52 -0700

    user mongoose queries

--- api_v2/api/users/query/index.js
+++ api_v2/api/users/query/index.js
@@ -1,1 +1,20 @@
 const User = require('./../model');
+
+const createUser = async userData => {
+	try {
+		const newUser = new User(userData);
+		return newUser.save();
+	} catch(error) {
+		return error;
+	}
+};
+
+const getUserByEmail = async email => {
+	try {
+		return await User.findOne({ email: email });
+	} catch(error) {
+		return error;
+	}
+};
+
+module.exports = { createUser, getUserByEmail };
