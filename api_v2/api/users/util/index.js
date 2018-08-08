5d9424b6c6c211e2aa177b2c0a6c57ba692079a6
Joshua Benz <joshua@planethull.com>
2018-08-07 22:23:30 -0700

    configured nev

--- api_v2/api/users/util/index.js
+++ api_v2/api/users/util/index.js
@@ -1,4 +1,10 @@
-const bcrypt = require('bcryptjs');
+const bcrypt 	= require('bcryptjs');
+const mongoose	= require('mongoose');
+const nev		= require('email-verification')(mongoose);
+const models	= require('./../model');
+
+const TempUser 	= models.tmpUser;
+const User		= models.user;
 
 const hashPassword = password => {
 	return new Promise((resolve, reject) => {
@@ -32,4 +38,56 @@ const requireAdmin = (request, response, nect) => {
 	next();
 };
 
+// hashing function for nev
+var hashFunction = function(password, tempUserData, insertTempUser, callback) {
+	bcrypt.genSalt(10, function(err, salt) {
+		bcrypt.hash(password, salt, function(err, hash) {
+			return insertTempUser(hash, tempUserData, callback);
+		});
+	});
+};
+
+nev.configure({
+	verificationURL: 'http://localhost:4200/#/auth/email-verification/${URL}', // set this when we make the front end for it	
+	persistentUserModel: User,
+	tempUserCollection: 'TempUsers',
+	URLFieldName: 'GENERATED_VERIFYING_URL',
+	tempUserModel: TempUser,
+
+	transportOptions: {
+		service: 'Gmail',
+		auth: {
+			user: 'erudition.testing@gmail.com',
+			pass: 'jb313327'
+		}
+	},
+	
+	hashingFunction: hashFunction,
+	passwordFieldName: 'password',
+
+	verifyMailOptions: {
+		from: 'Do Not Reply <erudition.testing_do_not_reply@gmail.com>',
+		subject: 'Please confirm account',
+		html: 'Click the following link to confirm your account:</p><p>${URL}</p>',
+		text: 'Please confirm your account by clicking the following link: ${URL}'
+	}
+}, function(error, options){
+	if (error) {
+		console.log(error);
+		return;
+	}
+	console.log('nev configured: ' + (typeof options === 'object'));
+});
+
+	nev.generateTempUserModel(TempUser, function(err, tempUserModel) {
+		if (err) {
+			console.log(err);
+			return;
+    	}
+	console.log('generated temp user model: ' + (typeof tempUserModel === 'function'));
+});
+
+
+
 module.exports = { hashPassword, verifyPassword, requireAdmin };
+module.exports.NEV = nev;
