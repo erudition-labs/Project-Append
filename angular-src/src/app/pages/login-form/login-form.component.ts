24cb1a3ab505e2b8b330e1ffd82fd29a0b43fba1
Joshua Benz <joshua@planethull.com>
2018-08-13 13:04:55 -0700

    login form

---
+++ angular-src/src/app/pages/login-form/login-form.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {
+	FormGroup,
+	FormBuilder,
+	FormControl,
+	Validators
+} from '@angular/forms'
+
+@Component({
+  selector: 'anms-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.css']
+})
+export class LoginFormComponent implements OnInit {
+	private loginForm: FormGroup;
+	private loginLoading = false;
+	private loginResult: any;
+
+	constructor(
+		private formBuilder : FormBuilder,
+		private router		: Router) { }
+
+	ngOnInit() {
+		this.createForm();
+	}
+
+	private createForm() : void {
+		this.loginForm = this.formBuilder.group({
+			email 		: new FormControl('', { validators: [Validators.required] }),
+			password	: new FormControl('', { validators: [Validators.required] })
+		});
+	}
+
+	public onSubmit() : void {
+		this.loginForm.controls.email.markAsDirty();
+		this.loginForm.controls.password.markAsDirty();
+
+		const { email, password } = this.loginForm.value;
+		const credentials : Credentials = {
+			email,
+			password
+		};
+
+		if(this.loginForm.valid) {
+			this.loginLoading = true;
+			this.login(credentials).subscribe(
+				result => {
+					this.loginLoading = false;
+					this.authService.setUser(
+						result.token,
+						result.userInfo,
+						result.expiresAt
+					);
+					this.router.navigate(['dashboard']);
+				}, error => {
+					this.loginResult = {
+						message: error.error.message,
+						state: 'error'
+					};
+					this.loginLoading = false;
+				}
+			);
+		}
+	}
+}
