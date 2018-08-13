44f55db1a629bddffc154bb08537d3d2c3a17e49
Joshua Benz <joshua@planethull.com>
2018-08-13 12:07:37 -0700

    changing template againa

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,46 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
 
+import { SharedModule } from '@app/shared';
+import { CoreModule } from '@app/core';
 
-import { AppRoutingModule } from './app.routing';
-import { ComponentsModule } from './components/components.module';
+import { SettingsModule } from './settings';
+import { StaticModule } from './static';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-//import { TableListComponent } from './table-list/table-list.component';
-//import { TypographyComponent } from './typography/typography.component';
-//import { IconsComponent } from './icons/icons.component';
-//import { MapsComponent } from './maps/maps.component';
-//import { NotificationsComponent } from './notifications/notifications.component';
-//import { UpgradeComponent } from './upgrade/upgrade.component';
-import {
-  AgmCoreModule
-} from '@agm/core';
-import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 
 @NgModule({
   imports: [
+    // angular
     BrowserAnimationsModule,
-    FormsModule,
-    HttpModule,
-    ComponentsModule,
-    RouterModule,
-    AppRoutingModule,
-    AgmCoreModule.forRoot({
-      apiKey: 'YOUR_GOOGLE_MAPS_API_KEY'
-    })
-  ],
-  declarations: [
-    AppComponent,
-    AdminLayoutComponent,
+    BrowserModule,
 
+    // core & shared
+    CoreModule,
+    SharedModule,
+
+    // features
+    StaticModule,
+    SettingsModule,
+
+    // app
+    AppRoutingModule
   ],
+  declarations: [AppComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
