2f2fcbcf8c2c2a874bd9421cf70933646f83b37d
Joshua Benz <joshua@planethull.com>
2018-08-13 22:29:45 -0700

    added services

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -1,67 +1,99 @@
-import { Injectable       } from '@angular/core';
-import { Http, Headers    } from '@angular/http';
-import { tokenNotExpired  } from 'angular2-jwt';
-import 'rxjs/add/operator/map';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { NewUser } from './../user/user.model';
+import { Credentials } from './../user/credentials.model';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class AuthService {
-    authToken : any;
-    user      : any;
+  constructor(public http: HttpClient, public router: Router) {}
 
-    constructor(private http:Http) { }
+  public isAuthenticated(): boolean {
+    // Get the time the token expires
+    const expiresAt = localStorage.getItem('expiresAt');
 
-    /*
-    Look at the users.js in the routes directory. We make the POST request to register
-    It then takes all the form fields and maps it to the newUser object, which is then
-    ran through the addUser()
-    */
-    registerUser(user) {
-        let headers = new Headers();
-        headers.append('Content-Type', 'application/json');
-            return this.http.post('http://localhost:3000/users/register', user, {headers: headers})
-            .map(response => response.json());
+    // If there's no expiresAt value, make
+    // the user log in
+    if (!expiresAt) {
+      return false;
     }
+    // Our indication as to whether the user is authenticated or not
+    // is if they have an unexpired token. Return a boolean that compares
+    // the current time with the token expiry time. The expiresAt value
+    // needs to be parsed because it is stored as a string
+    return new Date().getTime() < parseInt(expiresAt);
+  }
 
-    authenticateUser(user) {
-        let headers = new Headers();
-        headers.append('Content-Type', 'application/json');
-        return this.http.post('http://localhost:3000/users/authenticate', user, {headers: headers})
-        .map(response => response.json());
-    }
+  public login(credentials: Credentials): Observable<any> {
+    // We're using the spread syntax to get the
+    // individual properties off the supplied user
+    // object onto a new object
+    return this.http.post(`/api/authenticate`, { ...credentials });
+  }
 
-    getProfile() {
-        let headers = new Headers();
-        this.loadToken();
-        headers.append('Authorization', this.authToken);
-        headers.append('Content-Type', 'application/json');
-        return this.http.get('http://localhost:3000/users/profile', {headers: headers})
-        .map(response => response.json());
-    }
+  private setToken(token: string): void {
+    localStorage.setItem('token', token);
+  }
 
-    storeUserData(token, user) {
-        localStorage.setItem('id_token', token);
-        localStorage.setItem('user', JSON.stringify(user));
-        this.authToken  = token;
-        this.user       = user;
-        console.log('asdfasfasfasdfasdfasdfasd' + this.user);
-    }
+  public getToken(): string {
+    return localStorage.getItem('token');
+  }
 
-    loadToken() { //fetched from local sotrage
-        const token = localStorage.getItem('id_token');
-        this.authToken = token;
-    }
+  private setUserInfo(userInfo: any): void {
+    localStorage.setItem('userInfo', JSON.stringify(userInfo));
+  }
 
-    loggedIn() {
-        return tokenNotExpired('id_token');
-    }
+  private setExpiresAt(expiresAt: number): void {
+    // When we compare the current time against the expires at time
+    // later, we do so using a JavaScript Date object. The Date
+    // object deals with timestamps differently than how we have them
+    // in the token, so we need to multiple by 1000
+    localStorage.setItem('expiresAt', JSON.stringify(expiresAt * 1000));
+  }
 
-    logout() {
-        this.authToken = null;
-        this.user = null;
-        localStorage.clear();
-    }
+  public getUserInfo(): any {
+    return JSON.parse(localStorage.getItem('userInfo'));
+  }
 
-    isAdmin(user) {
-        return user.access === 'admin';
+  public setUser(token: string, userInfo: any, expiresAt: number): void {
+    // using local storage is only required
+    // when not using cookies to store the JWT
+    // this.setToken(token);
+    this.setUserInfo(userInfo);
+    this.setExpiresAt(expiresAt);
+  }
+
+  public signup(user: NewUser): Observable<any> {
+    // We're using the spread syntax to get the
+    // individual properties off the supplied user
+    // object onto a new object
+    return this.http.post(`/api/users`, { ...user });
+  }
+
+  // public logout(): Observable<any> {
+  //   return this.http.post('/api/logout', {});
+  // }
+
+  public logout(): void {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userInfo');
+    localStorage.removeItem('expiresAt');
+    this.router.navigate(['login']);
+  }
+
+  public isAdmin(): boolean {
+    const userInfo = this.getUserInfo();
+    if (!userInfo) {
+      return false;
     }
+    return userInfo.role === 'admin';
+  }
+
+  public userHasRole(expectedRole: string): boolean {
+    const userInfo = this.getUserInfo();
+    return expectedRole === userInfo.role;
+  }
 }
