319ec3f994e15889155194c778617bd5af7d419c
Joshua Benz <joshua@planethull.com>
2018-08-18 23:44:16 -0700

    updated model

--- api_v2/api/events/model/index.js
+++ api_v2/api/events/model/index.js
@@ -3,16 +3,21 @@ const Schema	= mongoose.Schema;
 
 const eventModel = new Schema({
 	name					: { type: String, required: true  },
-	isVerificationRequired	: { type: Boolean, required: true },
-	isVerified				: { type: Boolean, required: true },
-	isSignupRequired		: { type: Boolean, required: true },
+	isVerificationRequired	: { type: Boolean, required: true, default: true },
+	isVerified				: { type: Boolean, required: true, default: false },
+	isSignupRequired		: { type: Boolean, required: true, default: false },
 	startDate				: { type: Date, required: false },
 	endDate					: { type: Date, required: false },
-	OIC		: {
+	OIC		: {[
 		type: Schema.Types.ObjectId,
 		ref: 'User',
 		required: false
-	}
+	]},
+	signedUp : {[
+		type: Schema.Types.ObjectId,
+		ref: 'User,
+		required: false'
+	]}
 });
 
 module.exports = mongoose.model('event', eventModel);
