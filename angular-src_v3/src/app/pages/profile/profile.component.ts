b5e316f275edbf97e626e3fd311559b7ca717e39
Joshua Benz <joshua@planethull.com>
2018-08-21 15:44:12 -0700

    profile page

---
+++ angular-src_v3/src/app/pages/profile/profile.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../@core/data/users.service';
+
+@Component({
+	selector: 'profile',
+	templateUrl: './profile.component.html',
+	styleUrls: ['./profile.component.scss']
+})
+export class ProfileComponent implements OnInit {
+
+  constructor(private userService : UserService) { }
+
+  ngOnInit() {
+  /*this.userService.getProfile().subscribe((result) => {
+		console.log(result.data);
+	});*/
+  }
+
+}
