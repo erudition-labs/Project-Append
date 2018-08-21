9f97e55fb25c400b8865d8ec797456e0d7a42389
Joshua Benz <joshua@planethull.com>
2018-08-21 11:02:22 -0700

    fixed user service

--- angular-src_v3/src/app/@core/user/user.service.ts
+++ angular-src_v3/src/app/@core/user/user.service.ts
@@ -9,13 +9,13 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 export class UserService {
 	constructor(private http: HttpClient) {}
 
-	readonly url : string = "http://localhost:3000/api/v1/"
+	readonly url : string = "http://localhost:3000/api/v1/users"
 	public checkEmail(email: string) : Observable<any> {
 		const params = new HttpParams({
 			fromObject: {
 				email
 			}
 		});	
-		return this.http.get(`users/check-email`, { params });
+		return this.http.get(this.url + `/check-email`, { params });
 	}
 }
