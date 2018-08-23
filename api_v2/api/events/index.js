068ea589ef0d9a2bf1555cddd7daaf3310b9e7f6
Joshua Benz <joshua@planethull.com>
2018-08-22 20:44:31 -0700

    setup passport

--- api_v2/api/events/index.js
+++ api_v2/api/events/index.js
@@ -2,9 +2,10 @@ const express 			= require('express');
 const router	 		= express.Router();
 const controller 		= require('./controller');
 const { requireAdmin }	= require('./../users/util');
+const passport 			= require('passport');
 
 //router.route('/').get(controller.getEvents);
-router.route('/').post(controller.postEvent);
+router.route('/').post(passport.authenticate('jwt', {session: false}), controller.postEvent);
 router.route('/:id').get(controller.getEvent);
 router.route('/').get(controller.getEvents);
 router.route('/:id').put(controller.putEvent);
--- api_v2/server.js
+++ api_v2/server.js
@@ -1,22 +1,49 @@
 //require('dotenv').config(); so we can use env variables like this...
 //process.env.JWT_SECRET
 const express 		= require('express');
-const expressJwt 	= require('express-jwt');
 const mongoose 		= require('mongoose');
 const bodyParser 	= require('body-parser');
 const cors 			= require('cors');
-const csrf 			= require('csurf');
-const jwtDecode 	= require('jwt-decode');
-const jwt 			= require('jsonwebtoken');
 const helmet 		= require('helmet');
 const rateLimit 	= require('express-rate-limit');
+const passport		= require('passport');
+const JwtStrategy 	= require('passport-jwt').Strategy
+const ExtractJwt 	= require('passport-jwt').ExtractJwt;
 const config		= require('./database/config');
+const User			= require('./api/users/model').user;
+//require('./database/passport')(passport);
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+
+
+/* Passport Config */
+let options = {};
+options.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+options.secretOrKey = config.secret;
+//options.issuer = 'api.euriditionlabs.com';
+//options.audience = 'oursite.com';
+passport.use(new JwtStrategy(options, function(jwtPayload, done) {
+	User.findOne({ _id: jwtPayload.sub }, function(error, user) {
+		if(error) { return done(err, false); }
+
+		if(user) {
+			return done(null, user);
+		} else {
+			return done(null, false);
+			//or do something else like say hey..make an account
+		}
+	});
+}));
+			
+
+
+
+app.use(passport.initialize()); //passport middleware, authentication and token generation
+app.use(passport.session());   //will use passport-jwt strategy
 
 const limiter = new rateLimit({
 	windowMs: 15 * 60 * 1000, // 15 minutes
@@ -28,68 +55,12 @@ app.use(limiter);
 app.use(helmet());
 
 
-const attatchUser = (request, response, next) => {
-	const token = request.headers.authorization;
 
-	if(token === 'Bearer null' || !token) {
-		return response.status(401).json({success: false, message: 'Authentication Invalid'});
-	}
-	
-	const decodedToken = jwtDecode(token.replace('Bearer ', ''));
-	
-	if(!decodedToken) {
-		return response.status(401).json({success: false, message: 'There was a problem authorizing the request'});
-	} else {
-		request.user = decodedToken;
-		next();
-	}
-};
-/*
-const checkJwt = (request, response, next) => {
-	const token = request.headers.authorization;
-	console.log(token)
-	if(token === 'Bearer null' || !token) {
-		return response.status(403).json({success: false, message: 'Access denied'});
-	}
-	
-	try {
-		const decoded = jwt.verify(token, config.secret, { ///////////////////////change token secret to wahtevs
-			audience	: 'api.euriditionlabs.com',
-			issuer		: 'api.euriditionlabs.com'
-		});
-		console.log(decoded);
-		next();
-	} catch(error) {
-		//console.log(request.headers);
-		//console.log(error);
-		return response.status(403).json({success: false, message: 'Access denied'});
-	}
-};
-*/
-
-const checkJwt = expressJwt({ secret: config.secret });
-
-/*
- *
- ********* Routes that DO NOT Require Auth		
- *
- * */
 
 app.use('/api/v1/users', 			require('./api/users'));
 app.use('/api/v1/authenticate', 	require('./api/authenticate'));
+app.use('/api/v1/events', 			require('./api/events'));
 
-/*
- *
- ******** Routes REQUIRING Auth
- *
- * */
-
-app.use(attatchUser);
-app.use(checkJwt);
-
-app.use('/api/v1/events', require('./api/events'));
-
-//app.use('/api/something', require('./api.something'));
 
 async function connect() {
 	try {
