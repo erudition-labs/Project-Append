341d6f9c7037c9974afb042e310a9735c55d0897
Tristan <soldier@kenpokicks.com>
2018-09-10 17:29:22 -0700

    changed the buttons on the table

--- angular-src_v3/src/app/pages/user-management/user-management.component.ts
+++ angular-src_v3/src/app/pages/user-management/user-management.component.ts
@@ -10,6 +10,19 @@ export class UserManagementComponent implements OnInit {
 
   constructor() { }
   settings = {
+    add: {
+      addButtonContent: '<i class="nb-plus"></i>',
+      createButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    edit: {
+      editButtonContent: '<i class="nb-edit"></i>',
+      saveButtonContent: '<i class="nb-checkmark"></i>',
+      cancelButtonContent: '<i class="nb-close"></i>',
+    },
+    delete: {
+      deleteButtonContent: '<i class="nb-trash"></i>',
+    },
     columns: {
       id: {
         title: 'ID'
