75b7fe3d915b2fbd8a9652502340e42d716c8d6c
Tristan <soldier@kenpokicks.com>
2018-09-11 17:10:45 -0700

    changed header and footer and other such things

--- angular-src_v3/src/app/@theme/components/footer/footer.component.ts
+++ angular-src_v3/src/app/@theme/components/footer/footer.component.ts
@@ -6,9 +6,6 @@ import { Component } from '@angular/core';
   template: `
     <span class="created-by">Created by <b><a href="#" target="_blank">Erudition Labs</a></b> 2018</span>
     <div class="socials">
-      <a href="#" target="_blank" class="ion ion-social-github"></a>
-      <a href="#" target="_blank" class="ion ion-social-facebook"></a>
-      <a href="#" target="_blank" class="ion ion-social-twitter"></a>
       <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
     </div>
   `,
--- angular-src_v3/src/app/pages/dashboard/dashboard.module.ts
+++ angular-src_v3/src/app/pages/dashboard/dashboard.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
-
-
 import { ThemeModule } from '../../@theme/theme.module';
 import { DashboardComponent } from './dashboard.component';
+import { MatListModule } from '@angular/material';
 
 
 @NgModule({
   imports: [
     ThemeModule,
+    MatListModule,
   ],
   declarations: [
     DashboardComponent,
