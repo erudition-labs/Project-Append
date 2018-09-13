5712253a881451819d4d5ab0112671eddea33561
Tristan <soldier@kenpokicks.com>
2018-09-13 03:13:03 -0700

    backend routes work

--- api_v2/api/updates/controller/index.js
+++ api_v2/api/updates/controller/index.js
@@ -5,8 +5,14 @@ const mongoose 	= require('mongoose');
 const getUpdates = async(request, response) => {
     try {
         const updates = await queries.getUpdates();
+        console.log(updates[0].date);
+        
         response.json({ success: true, result: updates });
     } catch(error) {
         return error;
     }
 }
+
+module.exports = {
+	getUpdates,
+};
--- api_v2/api/updates/model/index.js
+++ api_v2/api/updates/model/index.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-const Scheme = mongoose.Scheme;
+const Schema = mongoose.Schema;
 
-const updateModel = new Scheme({
+const updateModel = new Schema({
     title       : { type: String, required: true },
     content     : { type: String, required: true },
-    author      : { type: mongoose.Scheme.Types.ObjectId, ref: 'user', required: true },
+    author      : { type: mongoose.Schema.Types.ObjectId, ref: 'user', required: true },
     date        : { type: Date, required: true }
 });
 
