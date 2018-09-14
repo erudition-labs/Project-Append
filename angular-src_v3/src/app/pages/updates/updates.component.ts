739a7c09256c23430c30f010051523671bb79836
Tristan <soldier@kenpokicks.com>
2018-09-13 19:25:38 -0700

    added fixed plus button and checks auth

--- angular-src_v3/src/app/pages/updates/updates.component.ts
+++ angular-src_v3/src/app/pages/updates/updates.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UpdatesService } from '../../@core/updates/updates.service';
 import { Update } from '../../@core/updates/update.model';
+import { AuthService } from '../../@core/auth/auth.service';
+import { ToastrService } from 'ngx-toastr';
 
 
 
@@ -11,11 +13,13 @@ import { Update } from '../../@core/updates/update.model';
 })
 export class UpdatesComponent implements OnInit {
 
-  constructor(private updatesService : UpdatesService) { }
+  constructor(private updatesService : UpdatesService, private authService : AuthService, private toast : ToastrService,) { }
 
   updates : Update[] = [];
   singleUpdate : Boolean = false;
   update : Update;
+  closeResult: string;
+
 
   ngOnInit() {
     this.updatesService.getUpdates().subscribe((result) => {
@@ -30,8 +34,38 @@ export class UpdatesComponent implements OnInit {
     this.singleUpdate = true;
     this.update = update;
   }
+
   private back() {
     this.singleUpdate = false; 
+  }
+
+  public openCreateDialog(): void {
+		if(this.authService.isAuthenticated() && this.authService.isAdmin()) {
+      this.success('You are authorized');
+		} else {
+			//tell them they no have access
+			this.error('You are not authorized');
+		}
+  }
+  
+  private error(msg : string) : void {
+		this.toast.error(msg, 'Error!', {
+			timeOut: 5000,
+			closeButton: true,
+			progressBar: true,
+			progressAnimation: 'decreasing',
+			positionClass: 'toast-top-right',
+		  });
+	}
+
+	private success(msg: string) : void {
+		this.toast.success(msg, 'Success!', {
+			timeOut: 5000,
+			closeButton: true,
+			progressBar: true,
+			progressAnimation: 'decreasing',
+			positionClass: 'toast-top-right',
+		  });
   }
 
 }
