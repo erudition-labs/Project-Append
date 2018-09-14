83999b1bc004e17da19b0e3e5edfbaafa85317f0
Joshua Benz <joshua@planethull.com>
2018-09-14 04:41:53 -0700

    clearing out old useless code

--- database/config/database.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	database: 'mongodb://localhost:27017/JROTC',
-	secret: 'lzmkKcnbfinngfeloifnlcrkymfkehlkemfkqsthlamacpd}mho`gtrojx(/mcrf' //use some randomly constantly changing ENV var for this in production
-}
