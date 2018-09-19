0fc7242cbb229f0e75bae95536efbd3c7bd2005b
Tristan <soldier@kenpokicks.com>
2018-09-18 20:34:31 -0700

    added changelog icon/link to footer, and editted the support us on the light and cosmic themes

--- angular-src/src/app/@theme/components/footer/footer.component.ts
+++ angular-src/src/app/@theme/components/footer/footer.component.ts
@@ -4,9 +4,11 @@ import { Component } from '@angular/core';
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by">Created by <b><a href="#" target="_blank">Erudition Labs</a></b> 2018</span>
+    <span class="created-by">Created by <b><a href="https://www.linkedin.com/company/eruditionlabs/" target="_blank">Erudition Labs</a></b> 2018</span>
+
     <div class="socials">
-      <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
+      <a href="#" target="_blank" class="ion ion-clipboard"></a>
+      <a href="https://www.linkedin.com/company/eruditionlabs/" target="_blank" class="ion ion-social-linkedin"></a>
     </div>
   `,
 })
