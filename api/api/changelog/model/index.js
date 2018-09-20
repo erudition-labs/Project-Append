c6ce37edd7fee007661b48764f1aea732bb9bf60
Joshua Benz <joshua@planethull.com>
2018-09-19 22:38:15 -0700

    forgot we dont need this now

--- api/api/changelog/model/index.js
+++
@@ -1,6 +0,0 @@
-const mongoose 	= require('mongoose');
-const Schema	= mongoose.Schema;
-const changelogModel = new Schema({
-    data: { type: String, required: true },
-});
-module.exports = mongoose.model('changelog', changelogModel);
