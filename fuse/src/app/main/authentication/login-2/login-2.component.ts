a706d89ad874e37a629aa5f18055f2c787e40ee3
Tristan <soldier@kenpokicks.com>
2018-09-28 18:50:25 -0700

    login works, token is stored and redirects to dashboard

--- fuse/src/app/main/authentication/login-2/login-2.component.ts
+++ fuse/src/app/main/authentication/login-2/login-2.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 import { FuseConfigService } from '@fuse/services/config.service';
 import { fuseAnimations } from '@fuse/animations';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../../@core/auth/auth.service';
+import { Credentials } from '../../../../@core/user/credentials.model';
 
 @Component({
     selector     : 'login-2',
@@ -13,7 +16,14 @@ import { fuseAnimations } from '@fuse/animations';
 })
 export class Login2Component implements OnInit
 {
-    loginForm: FormGroup;
+    public loginForm: FormGroup;
+	private loginLoading = false;
+	private loginResult: any;
+
+	public errors: string[] = [];
+	public messages: string[] = [];
+	public submitted: boolean = false;
+	private username: string;
 
     /**
      * Constructor
@@ -23,7 +33,10 @@ export class Login2Component implements OnInit
      */
     constructor(
         private _fuseConfigService: FuseConfigService,
-        private _formBuilder: FormBuilder
+        private formBuilder: FormBuilder,
+        private authService	: AuthService,
+		private router		: Router
+
     )
     {
         // Configure the layout
@@ -54,9 +67,62 @@ export class Login2Component implements OnInit
      */
     ngOnInit(): void
     {
-        this.loginForm = this._formBuilder.group({
-            email   : ['', [Validators.required, Validators.email]],
-            password: ['', Validators.required]
-        });
+        // this.loginForm = this._formBuilder.group({
+        //     email   : ['', [Validators.required, Validators.email]],
+        //     password: ['', Validators.required]
+        // });
+        this.createForm();
     }
+
+    private createForm() : void {
+		this.loginForm = this.formBuilder.group({
+			email 		: new FormControl('', { validators: [Validators.required, Validators.email] }),
+			password	: new FormControl('', { validators: [Validators.required] })
+		});
+	}
+
+	public onSubmit() : void {
+		this.errors = [];
+		this.messages = [];
+
+		this.loginForm.controls.email.markAsDirty();
+		this.loginForm.controls.password.markAsDirty();
+
+		const { email, password } = this.loginForm.value;
+		const credentials : Credentials = {
+			email,
+			password
+		};
+
+		if(this.loginForm.valid) {
+			this.loginLoading = true;
+			this.authService.login(credentials).subscribe(
+				result => {
+					if(result.success) {
+						this.loginLoading = false;
+						this.username = result.userInfo.firstName;
+						this.messages.push(result.message);
+
+						this.authService.setUser(
+							result.token,
+							result.userInfo,
+							result.expiresAt
+						);
+						setTimeout(() => {
+							this.router.navigate(['dashboard']);
+						}, 2000);
+					} else {
+						this.errors.push(result.message);
+					}
+				}, error => {
+					this.loginResult = {
+						message: error.error.message,
+						state: 'error'
+					};
+					this.errors.push(error.error.message);
+					this.loginLoading = false;
+				}
+			);
+		}
+	}
 }
