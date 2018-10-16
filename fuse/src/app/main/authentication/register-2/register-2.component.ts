d385292c73f33dddaee28f8a66bf1ac1ebbe3462
Tristan Hull <soldier@kenpokicks.com>
2018-10-16 16:56:41 -0700

    i have no clue

--- fuse/src/app/main/authentication/register-2/register-2.component.ts
+++ fuse/src/app/main/authentication/register-2/register-2.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators, FormControl } from '@angular/forms';
+import {
+    AbstractControl,
+    FormBuilder,
+    FormGroup,
+    ValidationErrors,
+    ValidatorFn,
+    Validators,
+    FormControl
+} from '@angular/forms';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
 import { FuseConfigService } from '@fuse/services/config.service';
 import { fuseAnimations } from '@fuse/animations';
-
 
 import { AuthService } from '../../../../@core/auth/auth.service';
 import { UserService } from '../../../../@core/user/user.service';
@@ -16,65 +23,63 @@ import { map } from 'rxjs/operators';
 export interface Select {
     value: string;
     viewValue: string;
-  }
+}
 
 @Component({
-    selector     : 'register-2',
-    templateUrl  : './register-2.component.html',
-    styleUrls    : ['./register-2.component.scss'],
+    selector: 'register-2',
+    templateUrl: './register-2.component.html',
+    styleUrls: ['./register-2.component.scss'],
     encapsulation: ViewEncapsulation.None,
-    animations   : fuseAnimations
+    animations: fuseAnimations
 })
-export class Register2Component implements OnInit, OnDestroy
-{
+export class Register2Component implements OnInit, OnDestroy {
     registerForm: FormGroup;
 
     public ranks: Select[] = [
-        {value: 'C/Ab', viewValue: 'Airman Basic'},
-        {value: 'C/Amn', viewValue: 'Airman'},
-        {value: 'C/A1C', viewValue: 'Airman First Class'}
-      ];
+        { value: 'C/Ab', viewValue: 'Airman Basic' },
+        { value: 'C/Amn', viewValue: 'Airman' },
+        { value: 'C/A1C', viewValue: 'Airman First Class' }
+    ];
     public teams: Select[] = [
-    {value: 'Rifle', viewValue: 'Rifle Team'},
-    {value: 'Sword', viewValue: 'Sword Team'},
-    {value: 'Color Guard', viewValue: 'Color Gaurd'},
-    {value: 'Drill', viewValue: 'Drill Team'}
+        { value: 'Rifle', viewValue: 'Rifle Team' },
+        { value: 'Sword', viewValue: 'Sword Team' },
+        { value: 'Color Guard', viewValue: 'Color Gaurd' },
+        { value: 'Drill', viewValue: 'Drill Team' }
     ];
     public flights: Select[] = [
-    {value: 'Alpha', viewValue: 'Alpha Flight'},
-    {value: 'Bravo', viewValue: 'Bravo Flight'},
-    {value: 'Charlie', viewValue: 'Charlie Flight'},
-    {value: 'Delta', viewValue: 'Delta Flight'}
+        { value: 'Alpha', viewValue: 'Alpha Flight' },
+        { value: 'Bravo', viewValue: 'Bravo Flight' },
+        { value: 'Charlie', viewValue: 'Charlie Flight' },
+        { value: 'Delta', viewValue: 'Delta Flight' }
     ];
 
     private _unsubscribeAll: Subject<any>;
-	public signupSuccess = false;
-	private emailValidating = false;
-    private signupResult : any;
-    public tempEmail : String;
+    public signupSuccess = false;
+    private emailValidating = false;
+    private signupResult: any;
+    public tempEmail: String;
 
-	public errors: string[] = [];
-	public messages: string[] = [];
-	public submitted: boolean = false;
+    public errors: string[] = [];
+    public messages: string[] = [];
+    public submitted = false;
 
     constructor(
         private _fuseConfigService: FuseConfigService,
         private formBuilder: FormBuilder,
-		private authService : AuthService,
-		private router		: Router,
-		private userService	: UserService
-    )
-    {
+        private authService: AuthService,
+        private router: Router,
+        private userService: UserService
+    ) {
         // Configure the layout
         this._fuseConfigService.config = {
             layout: {
-                navbar   : {
+                navbar: {
                     hidden: true
                 },
-                toolbar  : {
+                toolbar: {
                     hidden: true
                 },
-                footer   : {
+                footer: {
                     hidden: true
                 },
                 sidepanel: {
@@ -94,23 +99,25 @@ export class Register2Component implements OnInit, OnDestroy
     /**
      * On init
      */
-    ngOnInit(): void
-    {
+    ngOnInit(): void {
         // this.registerForm = this._formBuilder.group({
         //     name           : ['', Validators.required],
         //     email          : ['', [Validators.required, Validators.email]],
         //     password       : ['', Validators.required],
         //     passwordConfirm: ['', [Validators.required, confirmPasswordValidator]]
         // });
-        
+
         this.createForm();
 
         // Update the validity of the 'passwordConfirm' field
         // when the 'password' field changes
-        this.registerForm.get('password').valueChanges
-            .pipe(takeUntil(this._unsubscribeAll))
+        this.registerForm
+            .get('password')
+            .valueChanges.pipe(takeUntil(this._unsubscribeAll))
             .subscribe(() => {
-                this.registerForm.get('passwordConfirm').updateValueAndValidity();
+                this.registerForm
+                    .get('passwordConfirm')
+                    .updateValueAndValidity();
             });
     }
 
@@ -123,124 +130,133 @@ export class Register2Component implements OnInit, OnDestroy
         this._unsubscribeAll.complete();
     }
 
-    private createForm() : void {
+    private createForm(): void {
         this.registerForm = this.formBuilder.group({
             email: new FormControl('', {
-                validators: [Validators.required, Validators.email], 
+                validators: [Validators.required, Validators.email],
                 asyncValidators: [this.checkEmail.bind(this)],
                 updateOn: 'blur'
             }),
-            password			: new FormControl('', { validators: [Validators.required] }),
-            passwordConfirm     : new FormControl('', { validators: [Validators.required, confirmPasswordValidator]}),
-            firstName			: new FormControl('', { validators: [Validators.required] }),
-            lastName			: new FormControl('', { validators: [Validators.required] }),
-            rank				: new FormControl('', { }),
-            flight				: new FormControl('', { validators: [Validators.required] }),
-            team				: new FormControl('', { }),
-            role				: new FormControl('user', { }),
-            phone				: new FormControl('', { validators: [Validators.required] }),
-            isChangelogViewed 	: new FormControl(false, { }),
-            events				: new FormControl([], { })
+            password: new FormControl('', {
+                validators: [Validators.required]
+            }),
+            passwordConfirm: new FormControl('', {
+                validators: [Validators.required, confirmPasswordValidator]
+            }),
+            firstName: new FormControl('', {
+                validators: [Validators.required]
+            }),
+            lastName: new FormControl('', {
+                validators: [Validators.required]
+            }),
+            rank: new FormControl('', {}),
+            flight: new FormControl('', { validators: [Validators.required] }),
+            team: new FormControl('', {}),
+            role: new FormControl('user', {}),
+            phone: new FormControl('', { validators: [Validators.required] }),
+            isChangelogViewed: new FormControl(false, {}),
+            events: new FormControl([], {})
         });
 
         this.signupSuccess = false;
     }
 
-    private checkEmail(control: FormControl) : any {
-		this.emailValidating = true;
-		const email = control.value.toLowerCase();
+    private checkEmail(control: FormControl): any {
+        this.emailValidating = true;
+        const email = control.value.toLowerCase();
 
-		return this.userService.checkEmail(email).pipe(
-			map(
-				result => {
-					this.emailValidating = false;
-					if(result.emailTaken) {
-						return { emailTaken: true };
-					}
-					return null;
-				}, error => {
-					console.log(error);
-					this.emailValidating = false;
-				}
-			)
-		);
+        return this.userService.checkEmail(email).pipe(
+            map(
+                result => {
+                    this.emailValidating = false;
+                    if (result.emailTaken) {
+                        return { emailTaken: true };
+                    }
+                    return null;
+                },
+                error => {
+                    console.log(error);
+                    this.emailValidating = false;
+                }
+            )
+        );
     }
-    
-    public onSubmit() : void {
-		this.messages = [];
-		this.errors = [];
 
-		this.registerForm.controls.email.markAsDirty();
-		this.registerForm.controls.password.markAsDirty();
-		this.registerForm.controls.firstName.markAsDirty();
-		this.registerForm.controls.lastName.markAsDirty();
-		this.registerForm.controls.rank.markAsDirty();
-		this.registerForm.controls.flight.markAsDirty();
-		this.registerForm.controls.team.markAsDirty();
-		this.registerForm.controls.role.markAsDirty();
-		this.registerForm.controls.phone.markAsDirty();
-		this.registerForm.controls.events.markAsDirty();
+    public onSubmit(): void {
+        this.messages = [];
+        this.errors = [];
 
-		if(this.registerForm.valid) {
-			const {
-				email, 
-				password,
-				firstName,
-				lastName,
-				rank,
-				flight,
-				team,
-				role,
-				phone,
-				isChangelogViewed,
-				events
-			} = this.registerForm.value;
+        this.registerForm.controls.email.markAsDirty();
+        this.registerForm.controls.password.markAsDirty();
+        this.registerForm.controls.firstName.markAsDirty();
+        this.registerForm.controls.lastName.markAsDirty();
+        this.registerForm.controls.rank.markAsDirty();
+        this.registerForm.controls.flight.markAsDirty();
+        this.registerForm.controls.team.markAsDirty();
+        this.registerForm.controls.role.markAsDirty();
+        this.registerForm.controls.phone.markAsDirty();
+        this.registerForm.controls.events.markAsDirty();
 
-			const newUser : NewUser = {
-				email,
-				password,
-				firstName,
-				lastName,
-				rank,
-				flight,
-				team,
-				role,
-				phone,
-				isChangelogViewed,
-				events
-			};
-			
-			this.authService.signup(newUser).subscribe(
-				result => {
-					if(result.success) {
-						this.signupResult = {
-							message: result.message,
-							state: 'success'
-						};
+        if (this.registerForm.valid) {
+            const {
+                email,
+                password,
+                firstName,
+                lastName,
+                rank,
+                flight,
+                team,
+                role,
+                phone,
+                isChangelogViewed,
+                events
+            } = this.registerForm.value;
+
+            const newUser: NewUser = {
+                email,
+                password,
+                firstName,
+                lastName,
+                rank,
+                flight,
+                team,
+                role,
+                phone,
+                isChangelogViewed,
+                events
+            };
+
+            this.authService.signup(newUser).subscribe(
+                result => {
+                    if (result.success) {
+                        this.signupResult = {
+                            message: result.message,
+                            state: 'success'
+                        };
 
                         this.messages.push(result.message);
                         this.tempEmail = newUser.email;
-						this.signupSuccess = true;
+                        this.signupSuccess = true;
 
-						setTimeout(() => {
-							this.router.navigate(['login']);
-						}, 4000);
-					} else {
-                        this.errors.push(result.message);                        
-					}
-				}, error => {
-					this.signupResult = {
-						message: error.error.message,
-						state: 'error'
-					};
-					this.signupSuccess = false;
-				}
-			);
-		} else {
-            console.log("Not Valid");
-            
+                        setTimeout(() => {
+                            this.router.navigate(['login']);
+                        }, 4000);
+                    } else {
+                        this.errors.push(result.message);
+                    }
+                },
+                error => {
+                    this.signupResult = {
+                        message: error.error.message,
+                        state: 'error'
+                    };
+                    this.signupSuccess = false;
+                }
+            );
+        } else {
+            console.log('Not Valid');
         }
-	}
+    }
 }
 
 /**
@@ -249,30 +265,27 @@ export class Register2Component implements OnInit, OnDestroy
  * @param {AbstractControl} control
  * @returns {ValidationErrors | null}
  */
-export const confirmPasswordValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
-
-    if ( !control.parent || !control )
-    {
+export const confirmPasswordValidator: ValidatorFn = (
+    control: AbstractControl
+): ValidationErrors | null => {
+    if (!control.parent || !control) {
         return null;
     }
 
     const password = control.parent.get('password');
     const passwordConfirm = control.parent.get('passwordConfirm');
 
-    if ( !password || !passwordConfirm )
-    {
+    if (!password || !passwordConfirm) {
         return null;
     }
 
-    if ( passwordConfirm.value === '' )
-    {
+    if (passwordConfirm.value === '') {
         return null;
     }
 
-    if ( password.value === passwordConfirm.value )
-    {
+    if (password.value === passwordConfirm.value) {
         return null;
     }
 
-    return {'passwordsNotMatching': true};
+    return { passwordsNotMatching: true };
 };
