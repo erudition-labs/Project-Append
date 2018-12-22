3668413b7746e838fbdb7f59a4ad0f86c3502e8d
Joshua Benz <joshua@planethull.com>
2018-12-21 21:02:56 -0800

    user event signup actions

--- fuse/src/@core/store/users/users.actions.ts
+++ fuse/src/@core/store/users/users.actions.ts
@@ -14,7 +14,26 @@ export class LoadUsersFail {
     constructor(public readonly payload?: any) {}
 }
 
+//USER GOT ACCEPTED TO SIGNUP FOR EVENT
+export class UserEventSignup {
+    static readonly type = '[Users] User Event Signup';
+    constructor(public readonly payload: string) {}
+}
+
+export class UserEventSignupSuccess {
+    static readonly type = '[Users] User Event Signup Success';
+    constructor() {}
+}
+
+export class UserEventSignupFail {
+    static readonly type = '[Users] User Event Signup Fail';
+    constructor(public readonly payload?: any) {}
+}
+
 export type UsersActions = 
     | LoadUsers
     | LoadUsersSuccess
     | LoadUsersFail
+    | UserEventSignup
+    | UserEventSignupSuccess
+    | UserEventSignupFail;
