ed42953091af638c879a89276aaffdc28edcf3c1
Joshua Benz <joshua@planethull.com>
2019-01-27 09:54:07 -0800

    log when user connects

--- api/server.js
+++ api/server.js
@@ -14,8 +14,15 @@ const User			= require('./api/users/model').user;
 //require('./database/passport')(passport);
 
 const app 	= express();
-var http 	= require('http').Server(app);
-var io 		= require('socket.io')(http);
+const http 	= require('http').Server(app);
+const io 		= require('socket.io')(http);
+
+io.on('connection', function(socket) {
+	console.log('a user connected');
+	socket.on('disconnect', function() {
+	  console.log('user disconnected');
+	});
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
