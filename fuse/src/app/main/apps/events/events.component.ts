af95845b30b800e09a973fa63feff992be8f1ca3
Joshua Benz <joshua@planethull.com>
2019-01-28 16:45:13 -0800

    replace permissions with ngIf

--- fuse/src/app/main/apps/events/events.component.ts
+++ fuse/src/app/main/apps/events/events.component.ts
@@ -86,15 +86,8 @@ export class EventsComponent implements OnInit, OnDestroy {
         this.events$.pipe(takeUntil(this.ngUnsubscribe))
             .subscribe(e => this.events = e);
         this.refresh.next();
-        this.loadPermissions();
     }
 
-    private loadPermissions() : void {
-        this._permissionsService.flushPermissions();
-        this._permissionsService.addPermission('ADMIN', () => {
-            return ((this._tokenAuthService.isAuthenticated() && this._tokenAuthService.isAdmin()));
-        });
-    }
 
     beforeMonthViewRender({header, body}): void {
         // console.info('beforeMonthViewRender');
@@ -158,8 +151,6 @@ export class EventsComponent implements OnInit, OnDestroy {
                         });
                         break;
                 }
-
-                this.loadPermissions();
         });
     }
     
@@ -199,7 +190,6 @@ export class EventsComponent implements OnInit, OnDestroy {
                         break;
                     default: break;
                 }
-            this.loadPermissions();
         });
     }
 
@@ -224,7 +214,6 @@ export class EventsComponent implements OnInit, OnDestroy {
                             this._utils.success("Deleted");
                         });
                 }
-                this.loadPermissions();
             });
     }
 
