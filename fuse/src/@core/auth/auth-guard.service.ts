2643c30b3ace906ebb6b1ecc44550c55a24bb19a
Joshua Benz <joshua@planethull.com>
2019-02-04 15:28:21 -0800

    updated auth guard protected events route

--- fuse/src/@core/auth/auth-guard.service.ts
+++ fuse/src/@core/auth/auth-guard.service.ts
@@ -2,13 +2,19 @@ import { Injectable } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
 import { Store } from '@ngxs/store';
 import { AuthState } from '../store/auth/auth.state';
+import { TokenAuthService } from './tokenAuth.service'
 
 @Injectable()
 export class AuthGuardService implements CanActivate {
-  constructor(private _store: Store, public router: Router) {}
+  constructor(private _store: Store, 
+                public _router: Router,
+                private _tokenAuthService: TokenAuthService) {}
 
     canActivate(): boolean {
-        const token = this._store.selectSnapshot(AuthState);
-        return token !== undefined;
+        if(!this._tokenAuthService.isAuthenticated()) {
+            this._router.navigate(['login']);
+            return false;
+        }
+        return true;
     }
 }
